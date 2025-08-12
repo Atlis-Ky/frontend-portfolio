b1fff6cc8acd743a1090ea1ccd7ca65dab6c61d6
Ky <kylesmith97@outlook.com>
2025-08-12 14:38:21 +0100

    content updates, need to finish up fullstack app + update link

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -33,6 +33,10 @@ function ProjectList() {
       {/* DEVELOPER RESOURCES Section */}
       <section className="project-section dev-resources">
         <h2 className="section-title">DEVELOPER RESOURCES</h2>
+        <h4 className="section-subtitle">
+          Collection of simple but super useful tools and resources created for
+          frontend developers
+        </h4>
         <div className="button-grid">
           <a
             href="https://vscode-shortcuts-cheatsheet.vercel.app/"
@@ -64,6 +68,10 @@ function ProjectList() {
       {/* MISCELLANEOUS & WEBCLONES Section */}
       <section className="project-section misc-webclones">
         <h2 className="section-title">MISCELLANEOUS & WEBCLONES</h2>
+        <h4 className="section-subtitle">
+          Some extra projects I worked on to level up my skillset and practise
+          with common web development tools
+        </h4>
         <div className="button-grid">
           <a
             href="https://gym-app-react-atlisky.netlify.app/"
@@ -97,12 +105,32 @@ function ProjectList() {
           >
             AMAZON W/ BASKET
           </a>
+          <a
+            href="#"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-button"
+          >
+            TODO APP W/ Authentication (FULLSTACK)
+          </a>
+          <a
+            href="https://react-pokedex-atlisky.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="project-button"
+          >
+            POKEMON API DEX
+          </a>
         </div>
       </section>
 
       {/* CODING MUSIC Section */}
       <section className="project-section coding-music">
         <h2 className="section-title">CODING MUSIC</h2>
+        <h4 className="section-subtitle">
+          Ambient & Lo-Fi, ideal for working. Below is music I produced for this
+          purpose over the years, and some curated playlists
+        </h4>
         <div className="button-grid">
           <a
             href="#"
@@ -110,7 +138,7 @@ function ProjectList() {
             rel="noopener noreferrer"
             className="project-button"
           >
-            Atlis
+            Atlis (My Discography)
           </a>
           <a
             href="https://open.spotify.com/playlist/2HOAMKK6EifQ86uAL1SD9p?si=d55458e826db4ee8"
@@ -134,6 +162,10 @@ function ProjectList() {
       {/* CONTACT Section */}
       <section className="project-section contact">
         <h2 className="section-title">CONTACT</h2>
+        <h4 className="section-subtitle">
+          Find my code on Github, or reach out via LinkedIn / Email if you'd
+          like
+        </h4>
         <div className="button-grid">
           <a
             href="https://github.com/Atlis-Ky"
