841edc4c535e574802644e92dc832aa2d695a5b7
Ky <kylesmith97@outlook.com>
2025-08-13 15:37:56 +0100

    added section to header to display my current used technologies throughout portfolio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,27 @@ function Header() {
       <h1 className="mb-2">
         <span style={{ color: "var(--main-accent)" }}>ATLIS-KY</span> PROJECTS
       </h1>
+      <div className="tech-bar">
+        <span className="tech-item">React</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">Next.js</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">JavaScript</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">TypeScript</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">CSS & Tailwind</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">Bootstrap 5</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">Vitest</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">Node.js</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">Vite</span>
+        <span className="tech-separator">•</span>
+        <span className="tech-item">API</span>
+      </div>
     </header>
   );
 }
